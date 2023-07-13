6c43b99bcd5fee3011e314dbc9900c6b7d426af8
Ehsan <ehsanellahiofficial@gmail.com>
2023-07-13 11:12:12 +0500

    All Done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer>
+      <img src="/logo.jpg" alt="" />
+      <span>
+        <b> Developed by Ehsan & Ahtsham</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
